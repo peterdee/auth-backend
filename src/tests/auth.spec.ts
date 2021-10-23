f090c399dc63f0b85f93bdf3607e477240151028
Peter <p.dyumin@dunice.net>
2021-10-23 19:25:40 +0300

    Work on the tests

--- src/tests/auth.spec.ts
+++ src/tests/auth.spec.ts
@@ -1,11 +1,59 @@
 import { expect } from 'chai';
 
-import { ADMIN_EMAIL, ADMIN_PASSWORD, RESPONSE_MESSAGES } from '../configuration';
+import {
+  ADMIN_EMAIL,
+  ADMIN_PASSWORD,
+  RESPONSE_MESSAGES,
+  ROLES,
+} from '../configuration';
 import buildServer from '../server';
+import { User } from '../database';
+
+interface ResponseData {
+  tokens: {
+    access: string;
+    refresh: string;
+  };
+  user: User;
+}
+
+const TEST_EMAIL_INVALID = 'test';
+const TEST_EMAIL_VALID = 'test@email.com';
+const TEST_FIRST_NAME = 'Test';
+const TEST_LAST_NAME = 'Test';
+const TEST_PASSWORD = 'test';
 
 describe(
   'Test Auth controllers',
   (): void => {
+    it(
+      'Should sign user up',
+      async (): Promise<void> => {
+        const server = await buildServer();
+        const url = '/api/auth/sign-up';
+        const response: any = await server.inject({
+          method: 'POST',
+          payload: {
+            email: TEST_EMAIL_INVALID,
+            password: ADMIN_PASSWORD,
+          },
+          url,
+        });
+
+        await server.close();
+
+        // expect(response.statusCode).to.equal(200);
+
+        // const responseJSON = response.json();
+        // expect(responseJSON.info).to.eq(RESPONSE_MESSAGES.ok);
+        // expect(responseJSON.request).to.eq(`${url} [POST]`);
+
+        // const data: ResponseData = responseJSON.data;
+        // expect(data.tokens.access).to.exist;
+        // expect(data.tokens.refresh).to.exist;
+        // expect(data.user.role).to.eq(ROLES.admin);
+      },
+    );
     it(
       'Should sign user in',
       async (): Promise<void> => {
@@ -28,8 +76,10 @@ describe(
         expect(responseJSON.info).to.eq(RESPONSE_MESSAGES.ok);
         expect(responseJSON.request).to.eq(`${url} [POST]`);
 
-        // const { data: { tokens: TokenPayload, user } = {} } = responseJSON;
-        // expect(tokens).to.contain('access');
+        const data: ResponseData = responseJSON.data;
+        expect(data.tokens.access).to.exist;
+        expect(data.tokens.refresh).to.exist;
+        expect(data.user.role).to.eq(ROLES.admin);
       },
     );
   },
